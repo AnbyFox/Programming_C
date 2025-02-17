01704d87e4c0b451684c46b13d9d096345c03668
AnbyFox <155241517+AnbyFox@users.noreply.github.com>
2025-02-17 20:00:27 +1000

    Lab #2
    Has two tasks with massives.
    Change task_type to see each task.

---
+++ lab2/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+	
+	int task_type = 1;
+	// 1 - 1st task with 3x3 massive
+	// 2 - 2nd task with 2x2 massive 
+	
+	if(task_type == 1) {
+	    double M[3][3];
+	    
+	    int i, j;
+	    for(i = 0; i < 3; i++) {
+	    	for(j = 0; j < 3; j++) {
+	    		printf("Type value for %d %d argument: ", i, j);
+		        scanf("%lf", &M[i][j]);
+	    	}
+		}
+		
+		double a, b;
+		a = M[0][0] + M[1][1] + M[2][2];
+		b = M[2][0] + M[1][1] + M[0][2];
+		printf("\nMain horizontal line summ = %lf\n", a);
+		printf("Side horizontal line summ = %lf", b);
+	} else {
+		int M[2][2];
+		
+		int i, j;
+	  for(i = 0; i < 2; i++) {
+	    	for(j = 0; j < 2; j++) {
+	    		int num;
+	    		printf("Type value for %d %d argument: ", i, j);
+		        scanf("%d", &num);
+		        M[i][j] = num * num;
+	    	}
+		}
+		
+		printf("\nCurrent massive:\n");
+		for(i = 0; i < 2; i++) {
+	    	for(j = 0; j < 2; j++) {
+	    		printf("[%d]", M[i][j]);
+	    		if(j > 0) printf("\n");
+	    	}
+		}
+		
+	}
+    
+    return 0;
+}
