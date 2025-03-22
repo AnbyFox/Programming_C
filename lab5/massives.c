5febc8c55878a437c894bc6132f556fd92927d6b
Tik <155241517+AnbyFox@users.noreply.github.com>
2025-03-22 12:09:08 +1000

    Lab #5 - Update 4
    Reduced the code in "massive.c" module

--- lab5/massives.c
+++ lab5/massives.c
@@ -12,31 +12,14 @@ double** massiveOperation(int size, double **M1, double **M2, char operation) {
 		M[i] = (double*)malloc(n*sizeof(double));
 	}
 	
-	if(operation == '-') {
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				M[i][j] = M1[i][j] - M2[i][j];
-			}
-		}  	
-	} else if(operation == '*') {
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				M[i][j] = M1[i][j] * M2[i][j];
-			}
-		}  
-	} else if(operation == '/') {
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				M[i][j] = M1[i][j] / M2[i][j];
-			}
-		}  
-	} else {
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				M[i][j] = M1[i][j] + M2[i][j];
-			}
-		}  
-	}
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			M[i][j] = M1[i][j] + M2[i][j];
+			if(operation == '-') M[i][j] = M1[i][j] - M2[i][j];
+			if(operation == '*') M[i][j] = M1[i][j] * M2[i][j];
+			if(operation == '/') M[i][j] = M1[i][j] / M2[i][j];
+		}
+	}  
 	
 	return M;
 }
